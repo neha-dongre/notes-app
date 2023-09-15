5646692409d329631c3d5fd6596e983c70dd70ba
NDD <128886954+neha-dongre@users.noreply.github.com>
2023-09-15 17:16:01 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import WebPage from "./vision/WebPage";
+import Android from "./vision/Android";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  const [dimension, setDimension] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const checkDimension = () => {
+      setDimension(window.innerWidth);
+    };
+
+    window.addEventListener("resize", checkDimension);
+
+    return () => {
+      window.removeEventListener("resize", checkDimension);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {dimension > 500 ? (
+            <Route path="/" element={<WebPage />} />
+          ) : (
+            <Route path="/" element={<Android />} />
+          )}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
